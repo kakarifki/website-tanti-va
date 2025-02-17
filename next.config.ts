5f94354d061b873098893bff94d7d8c5828aa287
M Rifki Septiawan <rifkim91@gmail.com>
2025-02-17 13:36:46 +0700

    initial project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['ucarecdn.com'], // Tambahkan domain Uploadcare
+  },
 };
 
 export default nextConfig;
